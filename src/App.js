817e9fa8ea812c802444739b59e835c064ae1ed9
Halston <sellenth@oregonstate.edu>
2020-03-25 17:32:22 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Hello extends React.Component {
+  render () {
+    return (
+      <div>
+        wassup {this.props.name}
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
